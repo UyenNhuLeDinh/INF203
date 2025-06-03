38548f08bf9da5ad0b3cece48bc9fd04fc539237
Nhu Dinh <le.uyen.nhu.dinh@nmbu.no>
2025-06-03 16:38:46 +0200

    Add position method to class Molecule

--- src/ljts/molecule.py
+++ src/ljts/molecule.py
@@ -1,9 +1,14 @@
-class Molecule:
-    def __init__(self, position):
-        """_summary_
 
-        Args:
-            position (_tuple_): Coordinates of a point mass in 3 dimensional space.
-        """
-        self._mass = 1
-        self.position = position
+class Molecule:
+    def __init__(self, x: float, y: float, z: float):
+        self._m = 1.0
+        self._x = x
+        self._y = y
+        self._z = z
+        
+    def position(self):
+        "Method to return the position, updated when x,y,z coordinates change."
+        self._position = (self._x, self._y, self._z)
+    
+        
+    
