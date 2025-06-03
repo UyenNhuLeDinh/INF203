f4d855d3e0546bc1a7a4e217414da5504a1dc2ab
Nhu Dinh <le.uyen.nhu.dinh@nmbu.no>
2025-06-03 17:17:02 +0200

    Develop populating method but have bugs

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from src.ljts.box import Box
 
 def main(): 
-    Lx = 5
+    Lx = 5.0
     Ly = 40
     rho_liquid = 0.73
-    rho_vapour = 0.02
+    rho_vapor = 0.02
     
-    our_box = Box(Lx, Ly)
-    our_box.compartment
-    our_box.density_to_compartment(rho_liquid, rho_vapour)
-    
-    
+    our_box = Box(Lx, Ly, rho_liquid, rho_vapor)
+    print(our_box._compartments)
+    print('The total number of molecules: ', len(our_box._molecules))
     
     
-if __name__ == "__main__" :
+    
+if __name__ == "__main__":
     main() 
     
     
--- src/ljts/box.py
+++ src/ljts/box.py
@@ -1,15 +1,32 @@
+import random
 from .molecule import Molecule
 
 class Box:
-    def __init__(self, Lx, Ly):
+    def __init__(self, Lx: float, Ly: float, rho_liquid: float, rho_vapor: float):
+        # Lengths of the box:
         self._Lx = Lx
         self._Ly = Ly
         self._Lz = Lx
-        self._V = self._Ly * self._Lx **2
-        self._molecules = []
+        
+        # Density at different phases:
+        self._rho_liquid = rho_liquid
+        self._rho_vapor = rho_vapor
+        
+        # Volume of the box:
+        self._V = self._Lx * self._Ly * self._Lz
+        
+        # Dictionary stores volumes of three compartments:
         self._compartments = {}
-        self._densities = {}
         self.compartment()
+        
+        # Dictionary stores densities of three compartments:
+        self._densities = {}
+        
+        # List stores all molecules' coordinates in the box:
+        self._molecules = []
+        
+        self.compartment()
+        
         
     def compartment(self):
         self._lower = 0.4 * self._V
@@ -20,21 +37,40 @@ class Box:
         "lower": self._lower,
         "middle": self._middle,
         "upper": self._upper
-    }
+        }
         
-    def density_to_compartment(self, rho_liquid, rho_vapour):
+        
+    def compartment_densities(self):
         self._densities = {
-            "lower": rho_vapour,
-            "middle": rho_liquid,
-            "upper": rho_vapour
+            "lower": self._rho_vapor,
+            "middle": self._rho_liquid,
+            "upper": self._rho_vapor
             }
         
-    def compute_nr_molecule(self):
-        total_molecules = 0
+        
+    def populate_box(self):
+        # Call assigning density method:
+        self.compartment_densities()
+        
+        # Loop through each items in self._densities (region and density):
         for region, density in self._densities.items():
             volume = self._compartments[region]
-            nr_molecules = density * volume
-            total_molecules += nr_molecules
-        print("The total number of molecules in the box:", total_molecules)
+            # Compute total number of molecules in each compartment:
+            num_molecules = int(density * volume)
+            
+            # Populate molecules in specific region:
+            if region == "lower":
+                y_min, y_max = 0, 0.4 * self._Ly
+            elif region  == "middle":
+                y_min, y_max = 0.4 * self._Ly, 0.6 * self._Ly
+            else: # "upper" region
+                y_min, y_max = 0.6 * self._Ly, self._Ly
+            
+            for _ in range(num_molecules):
+                x = random.uniform(0, self._Lx)
+                y = random.uniform(y_min, y_max)
+                z = random.uniform(0, self._Lz)
+                self._molecules.append(Molecule(x,y,z))
+        
 
             
