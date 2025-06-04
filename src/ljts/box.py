a3f11977a10a47a414c14948470c2e177c9ff7f9
Nhu Dinh <le.uyen.nhu.dinh@nmbu.no>
2025-06-04 21:46:05 +0200

    Added docstrings to Box

--- src/ljts/box.py
+++ src/ljts/box.py
@@ -4,6 +4,17 @@ from .molecule import Molecule
 
 class Box:
     def __init__(self, Lx: float, Ly: float, rho_liquid: float, rho_vapor: float):
+        """Create the simulation cuboid with given dimensions and phase densities.
+
+        Args:
+            Lx (float): Length of the box in the x and z directions (box is square in xz).
+            Ly (float): Length of the box in the y direction.
+            rho_liquid (float): Initial density of molecules in the liquid compartment.
+            rho_vapor (float): Initial density of molecules in the vapor compartment.
+            
+        Raises:
+            ValueError: If any of dimensions and densities are non-positive.
+        """
         # Lengths of the box:
         self._Lx = Lx
         self._Ly = Ly
@@ -13,10 +24,13 @@ class Box:
         self._rho_liquid = rho_liquid
         self._rho_vapor = rho_vapor
         
+        if Lx <= 0 or Ly <= 0 or rho_liquid < 0 or rho_vapor < 0:
+            raise ValueError("Box dimensions and densities must be positive.")
+        
         # Volume of the box:
         self._V = self._Lx * self._Ly * self._Lz
         
-        # Cut-off radius:
+        # Cut-off radius for Lennard-Jones (LJ) Potential:
         self._cutoff = 2.5
         
         # Dictionary stores volumes of three compartments:
@@ -31,6 +45,11 @@ class Box:
         
         
     def compartment(self):
+        """Define compartment volumes and store them in self._compartments.
+        Note: 
+            The box is divided along the y-axis into three regions: 
+            lower (40%), middle (20%), and upper (40%).
+        """
         self._compartments = {
         "lower": 0.4 * self._V,
         "middle": 0.2 * self._V,
@@ -39,6 +58,8 @@ class Box:
         
         
     def compartment_densities(self):
+        """Assign densities to compartments and store them in self._densities.
+        """
         self._densities = {
             "lower": self._rho_vapor,
             "middle": self._rho_liquid,
@@ -47,6 +68,11 @@ class Box:
         
         
     def populate_box(self):
+        """Populate the simulation box with molecules randomly placed in each compartment.
+            The number of molecules is the product of densities and compartment volumes.
+        Note:
+            The function prints the total number of molecules in the box.
+        """
         # Call assigning density method:
         self.compartment_densities()
         
@@ -64,17 +90,31 @@ class Box:
             else: # "upper" region
                 y_min, y_max = 0.6 * self._Ly, self._Ly
             
-            # Random configuration generation on positions of molecules:
+            # Random generation of the molecules' positions:
             for _ in range(num_molecules):
                 x = random.uniform(0, self._Lx)
                 y = random.uniform(y_min, y_max)
                 z = random.uniform(0, self._Lz)
                 self._molecules.append(Molecule(x,y,z))
                 
-        print('The total number of molecules: ', len(self._molecules))        
+        print('The total number of molecules: ', len(self._molecules))       
                 
                 
     def compute_potential(self):
+        """Compute the total potential energy of the box using the pairwise 
+        Lennard-Jones truncated-shifted (LJTS) energy.
+        
+        Returns:
+            float: The total potential energy of the box.
+            
+        Raises:
+            ValueError: If no molecules are initialized in the box.     
+        
+        Notes:
+            - Assumes all molecules have unit mass of 1.
+            - Only pairs within a cutoff radius of 2.5 are considered.
+            - The LJ potential is shifted to ensure continuity at the cutoff.     
+        """
         # Get positions of all molecules:
         positions = [(mol._x, mol._y, mol._z) for mol in self._molecules]
             
@@ -82,18 +122,40 @@ class Box:
         N = len(positions)
         
             
-        def LJ_potential(r_ij):
-            Epot_off = 4 * ((1 / self._cutoff ** 12) - (1 / self._cutoff** 6))
+        def LJ_potential(r_ij: float):
+            """Calculate Lennard-Jones potential energy between a pair of molecules.
+            
+            Args:
+                r_ij (float): The distance between molecule i and j.
+                
+            Returns:
+                float: Pairwise Lennard-Jones energy with truncation and shift.
+            """
+            shift = 4 * ((1 / self._cutoff ** 12) - (1 / self._cutoff** 6))
             
             if r_ij ** 2 < self._cutoff ** 2:
                 inv_r6 = r_ij ** -6
                 inv_r12 = inv_r6 ** 2
-                return 4 * (inv_r12 - inv_r6) - Epot_off
+                return 4 * (inv_r12 - inv_r6) - shift
             else:
                 return 0
             
+            
         def pbc_distance(dx: float, L: float):
+            """Apply minimum image convention for periodic boundary conditions.
+            
+            Args:
+                dx (float): Displacement along one coordinate axis (xj - xi).
+                L (float): Length of the periodic box in that direction.
+                
+            Returns:
+                float: The shortest distance between two molecules' image on a particular direction.
+                
+            Notes:
+                round(dx/L) gives the nearest image of the second molecule.
+            """
             return dx - round(dx / L) * L
+        
         
         # Iterations to cummulate the potential energy:
         E_pot = 0
